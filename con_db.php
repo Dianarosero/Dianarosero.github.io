5322761d003057c17fee5cf7afa05a1dd45f2e4b
Diana Sofía Rosero <dirolo.dr@gmail.com>
2023-04-19 11:02:40 -0500

    conexion good

--- con_db.php
+++ con_db.php
@@ -1,5 +1,16 @@
 <?php
 
-$conex = mysqli_connect("localhost","root","","registro"); 
+$conex = mysqli_connect("localhost","root","","registro")or die(mysql_error($mysqli)); 
 
+insertar($conex);
+
+function insertar($conex){
+    $name=$_POST['names'];
+    $email=$_POST['email'];
+
+    $consulta="INSERT INTO datos(nombre, email) 
+    VALUES ('$name','$email')";
+    mysqli_query($conex,$consulta);
+    mysqli_close($conex);
+}
 ?>
